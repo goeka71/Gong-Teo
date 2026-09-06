d30ab942937ab8c0167730cdd4a482cacb22d47f
kikiji040818-cloud <kikiji040818@ewha.ac.kr>
2026-09-06 15:23:16 +0900

    회원가입 주소 추가

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.user_list, name="user-list"),
+    path("signup/", views.signup, name="signup"),
     path("coin-history/", views.coin_history_list, name="coin-history-list"),
 ]
