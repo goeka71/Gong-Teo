3010b111b5e10f89aed9f869c9023995ad1ef80a
kikiji040818-cloud <kikiji040818@ewha.ac.kr>
2026-09-06 15:35:40 +0900

    로그인 url 추가

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from . import views
 
 
 urlpatterns = [
     path("", views.user_list, name="user-list"),
     path("signup/", views.signup, name="signup"),
+
+    path(
+        "login/",
+        TokenObtainPairView.as_view(),
+        name="token-obtain-pair",
+    ),
+
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token-refresh",
+    ),
+
     path("coin-history/", views.coin_history_list, name="coin-history-list"),
 ]
