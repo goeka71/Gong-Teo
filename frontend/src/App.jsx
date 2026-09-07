36ffcee1a8a2312b25181346ea5afab070327198
kikiji040818-cloud <kikiji040818@ewha.ac.kr>
2026-09-07 23:17:29 +0900

    app.jsx 불가피하게 수정했음. 노션 참고바람..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import FacilityDetail from "./facilities/FacilityDetail";
 import OnedayBoard from "./oneday/OnedayBoard";
 import Login from "./user/Login";
 import MyPage from "./user/MyPage";
+import Signup from "./user/Signup";
 
 // 공통 레이아웃: 모든 화면 위에 상단바(Navbar)를 두고,
 // 그 아래(Outlet)에 현재 경로에 해당하는 화면을 끼워 넣는다.
@@ -34,6 +35,7 @@ function App() {
         <Route path="/oneday" element={<OnedayBoard />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Route>
     </Routes>
   );
