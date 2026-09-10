1776ed1419595e16e9e187d613428409973211ac
haedam <haedam0710@naver.com>
2026-09-10 13:55:48 +0900

    view.py 충돌 없앰

--- facilities/views.py
+++ facilities/views.py
@@ -1,6 +1,8 @@
 from django.db.models import F
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from .models import (
